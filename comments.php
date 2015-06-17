2ec8ed5c843e031c2d3da917d28bd38ea537b5c8
Mte90 <mte90smanettator@gmail.com>
2015-06-17 23:20:08 +0200

    comment style of bootstrap

--- comments.php
+++ comments.php
@@ -11,30 +11,31 @@
  * @subpackage BootstrapWP
  */
 // Return early no password has been entered for protected posts.
-if (post_password_required()) {
-    return;
-} ?>
+if ( post_password_required() ) {
+	return;
+}
+?>
 <div id="comments" class="comments-area">
-    <?php if (have_comments()) : ?>
+<?php if ( have_comments() ) : ?>
 
-        <ul class="media-list">
-            <?php wp_list_comments(); ?>
-        </ul>
+	    <ul class="media-list">
+	<?php wp_list_comments( array( 'callback' => 'bootstrapwp_comment' ) ); ?>
+	    </ul>
 
-        <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
-            <nav id="comment-nav-below" class="navigation" role="navigation">
-                <div class="nav-previous">
-                    <?php previous_comments_link( _e('&larr; Older Comments')); ?>
-                </div>
-                <div class="nav-next">
-                    <?php next_comments_link(_e('Newer Comments &rarr;')); ?>
-                </div>
-            </nav>
-        <?php endif; // check for comment navigation ?>
+	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+		    <nav id="comment-nav-below" class="navigation" role="navigation">
+			<div class="nav-previous">
+		<?php previous_comments_link( _e( '&larr; Older Comments' ) ); ?>
+			</div>
+			<div class="nav-next">
+		<?php next_comments_link( _e( 'Newer Comments &rarr;' ) ); ?>
+			</div>
+		    </nav>
+	    <?php endif; // check for comment navigation  ?>
 
-        <?php elseif (!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type(), 'comments')) : ?>
-            <p class="nocomments"><?php _e('Comments are closed.'); ?></p>
+    <?php elseif ( !comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+	    <p class="nocomments"><?php _e( 'Comments are closed.' ); ?></p>
     <?php endif; ?>
 
-    <?php comment_form(); ?>
+<?php comment_form(); ?>
 </div>
--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 //Add thumbnail support
 add_theme_support( 'post-thumbnails' );
 
@@ -273,3 +272,71 @@ function bootstrap3_comment_form( $args ) {
 
 	return $args;
 }
+
+function bootstrapwp_comment( $comment, $args, $depth ) {
+	$GLOBALS[ 'comment' ] = $comment;
+	switch ( $comment->comment_type ) :
+		case 'pingback' :
+		case 'trackback' :
+			?>
+			<li class="comment media" id="comment-<?php comment_ID(); ?>">
+			    <div class="media-body">
+				<p>
+				    <?php _e( 'Pingback:' ); ?> <?php comment_author_link(); ?>
+				</p>
+			    </div>
+			    <?php
+			    break;
+		    default :
+			    // Proceed with normal comments.
+			    global $post;
+			    ?>
+
+			<li class="comment media" id="li-comment-<?php comment_ID(); ?>">
+			    <a href="<?php echo $comment->comment_author_url; ?>" class="pull-left">
+				<?php echo get_avatar( $comment, 64 ); ?>
+			    </a>
+			    <div class="media-body">
+				<h4 class="media-heading comment-author vcard">
+				    <?php
+				    printf( '<cite class="fn">%1$s %2$s</cite>', get_comment_author_link(),
+					    // If current post author is also comment author, make it known visually.
+					    ($comment->user_id === $post->post_author) ? '<span class="label"> ' . __(
+							    'Post author'
+						    ) . '</span> ' : ''  );
+				    ?>
+				</h4>
+
+				<?php if ( '0' == $comment->comment_approved ) : ?>
+					<p class="comment-awaiting-moderation"><?php
+					    _e(
+						    'Your comment is awaiting moderation.'
+					    );
+					    ?></p>
+				<?php endif; ?>
+
+				<?php comment_text(); ?>
+				<p class="meta">
+				    <?php
+				    printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>', esc_url( get_comment_link( $comment->comment_ID ) ), get_comment_time( 'c' ), sprintf(
+						    __( '%1$s at %2$s' ), get_comment_date(), get_comment_time()
+					    )
+				    );
+				    ?>
+				</p>
+				<p class="reply">
+				    <?php
+				    comment_reply_link( array_merge( $args, array(
+					'reply_text' => __( 'Reply <span>&darr;</span>' ),
+					'depth' => $depth,
+					'max_depth' => $args[ 'max_depth' ]
+						    )
+				    ) );
+				    ?>
+				</p>
+			    </div>
+			    <?php
+			    break;
+	    endswitch;
+    }
+    
